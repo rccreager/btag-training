60f25432c51819c33d0dd3c1f231963db4429202
Rachael Creager <rccreager@gmail.com>
2017-05-25 09:49:48 -0700

    adding roc curve to plot

--- plot.py
+++ plot.py
@@ -1,2 +1,21 @@
 import matplotlib as plt
 import sklearn
+
+
+fpr, tpr, _ = roc_curve(true_labels, pred_labels)
+auc = auc(fpr,tpr)
+
+def make_roc():
+    fig = plt.figure(1)
+    plt.title('ROC Curve')
+    plt.plot(fpr, trp, 'b', label='AUC = %0.2f'% auc)
+    plt.legend(loc='lower right')
+    plt.plot([0,1],[0,1],'r--')
+    plt.xlim([-0.1,1.2])
+    plt.ylim([-0.1,1.2])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    fig.savefig('ROC.pdf')
+
+
