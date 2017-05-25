f72b0c2cda5972b2923e7088e2636fd0527ebb52
Rachael Creager <rccreager@gmail.com>
2017-05-24 22:11:31 -0700

    adding setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+
+#assuming you have conda...
+condaconda create --name btagFresh
+source activate btagFresh
+conda install python=3.6.1
+conda install h5py
+conda install keras
+conda install -c conda-forge keras=2.0.2
+
